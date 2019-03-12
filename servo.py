520490804b8c7787f583e59bc69041d88b5e3859
bobjoefrank <phillyp.lai@gmail.com>
2019-03-12 03:21:33 -0700

    used pigpio library instead rpi to fix glitchy servos

--- servo.py
+++ servo.py
@@ -1,6 +1,7 @@
+# start daemon with $ sudo pigpiod
 import RPi.GPIO as GPIO
 import time
-
+import pigpio
 
 # letter to pins mapping
 # pins start from top left and goes down column and then next row
@@ -43,54 +44,97 @@ braille_map = {
         "9" : [False, True, False, True, False, False]
         }
 
-GPIO.setmode(GPIO.BOARD)
+#GPIO.setmode(GPIO.BOARD)
 
-channel_list = [40, 37, 38, 35, 36,33]
-GPIO.setup(channel_list, GPIO.OUT)
+#channel_list = [40, 37, 38, 35, 36, 33]
+#GPIO.setup(channel_list, GPIO.OUT)
 
-pin1 = GPIO.PWM(40, 50)
-pin2 = GPIO.PWM(38, 50)
-pin3 = GPIO.PWM(36, 50)
-pin4 = GPIO.PWM(37, 50)
-pin5 = GPIO.PWM(35, 50)
-pin6 = GPIO.PWM(33, 50)
+#pin1 = GPIO.PWM(40, 50)
+#pin2 = GPIO.PWM(38, 50)
+#pin3 = GPIO.PWM(36, 50)
+#pin4 = GPIO.PWM(37, 50)
+#pin5 = GPIO.PWM(35, 50)
+#pin6 = GPIO.PWM(33, 50)
 
-pin1.start(7.5)
-pin2.start(7.5)
-pin3.start(7.5)
-pin4.start(7.5)
-pin5.start(7.5)
-pin6.start(7.5)
+#pin1.start(7.5)
+#pin2.start(7.5)
+#pin3.start(7.5)
+#pin4.start(7.5)
+#pin5.start(7.5)
+#pin6.start(7.5)
+
+pi = pigpio.pi()
 
 #pin layout 
 # 40 37
 # 38 35
 # 36 33
+
+#add something to keep the pins the same state instead of reassigning new duty cycle
+#gets all glitchy if reassigned
+#7.5, 5.5
+
+temp0 = False
+temp1 = False 
+temp2 = False
+temp3 = False
+temp4 = False
+temp5 = False
 def letter_to_pins(letter):
+    global temp0
+    global temp1
+    global temp2
+    global temp3
+    global temp4
+    global temp5
     mapping = braille_map[letter.lower()]
-    if mapping[0] == True:
-        pin1.ChangeDutyCycle(7.5)
-    else:
-        pin1.ChangeDutyCycle(5.5)
-    if mapping[1] == True:
-        pin2.ChangeDutyCycle(7.5)
-    else:
-        pin2.ChangeDutyCycle(5.5)
-    if mapping[2] == True:
-        pin3.ChangeDutyCycle(7.5)
-    else:
-        pin3.ChangeDutyCycle(5.5)
-    if mapping[3] == True:
-        pin4.ChangeDutyCycle(7.5)
-    else:
-        pin4.ChangeDutyCycle(5.5)
-    if mapping[4] == True:
-        pin5.ChangeDutyCycle(7.5)
-    else:
-        pin5.ChangeDutyCycle(5.5)
-    if mapping[5] == True:
-        pin6.ChangeDutyCycle(7.5)
-    else:
-        pin6.ChangeDutyCycle(5.5)
+    if mapping[0] != temp0:
+        temp0 = mapping[0]
+        if mapping[0] == True:
+            pi.set_servo_pulsewidth(21, 1500)
+            #pin1.ChangeDutyCycle(7.5)
+        else:
+            pi.set_servo_pulsewidth(21, 1150)
+            #pin1.ChangeDutyCycle(7.5)
+    if mapping[1] != temp1:
+        temp1 = mapping[1]
+        if mapping[1] == True:
+            pi.set_servo_pulsewidth(20, 1500)
+            #pin2.ChangeDutyCycle(7.5)
+        else:
+            pi.set_servo_pulsewidth(20, 1150)
+            #pin2.ChangeDutyCycle(7.5)
+    if mapping[2] != temp2:
+        temp2 = mapping[2]
+        if mapping[2] == True:
+            pi.set_servo_pulsewidth(16, 1500)
+            #pin3.ChangeDutyCycle(7.5)
+        else:
+            pi.set_servo_pulsewidth(16, 1150)
+            #pin3.ChangeDutyCycle(7.5)
+    if mapping[3] != temp3:
+        temp3 = mapping[3]
+        if mapping[3] == True:
+            pi.set_servo_pulsewidth(26, 1500)
+            #pin4.ChangeDutyCycle(7.5)
+        else:
+            pi.set_servo_pulsewidth(26, 1150)
+            #pin4.ChangeDutyCycle(7.5)
+    if mapping[4] != temp4:
+        temp4 = mapping[4]
+        if mapping[4] == True:
+            pi.set_servo_pulsewidth(19, 1500)
+            #pin5.ChangeDutyCycle(7.5)
+        else:
+            pi.set_servo_pulsewidth(19, 1150)
+            #pin5.ChangeDutyCycle(7.5)
+    if mapping[5] != temp5:
+        temp5 = mapping[5]
+        if mapping[5] == True:
+            pi.set_servo_pulsewidth(13, 1500)
+            #pin6.ChangeDutyCycle(7.5)
+        else:
+            pi.set_servo_pulsewidth(13, 1150)
+            #pin6.ChangeDutyCycle(7.5)
     # time delay causes lag in system for pins to move
     time.sleep(1)
