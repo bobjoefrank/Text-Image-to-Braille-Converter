3046453c1092631a5b8e31da44e39447c9ebf85d
bobjoefrank <phillyp.lai@gmail.com>
2019-03-12 07:58:14 -0700

    image processing improvements

--- letter_extraction_pi.py
+++ letter_extraction_pi.py
@@ -13,10 +13,10 @@ from servo import *
 
 # bunch of values we can vary
 surfHessian = 500
-#width = 640
-#height = 480
 width = 320
-height=240
+height = 240
+box_height = 24
+box_width = 17
 
 def main():
     
@@ -26,7 +26,7 @@ def main():
     # initialize the camera and grab a reference to the raw camera capture
     camera = PiCamera()
     camera.resolution = (width, height)
-    camera.framerate = 30
+    camera.framerate = 20
     rawCapture = PiRGBArray(camera, size=(width, height))
     
     # allow the camera to warmup
@@ -39,35 +39,46 @@ def main():
         image = frame.array
     
         # image processing
-        crop_img = image[(int)(height/2-30):(int)(height/2+30), (int)(width/2-18):(int)(width/2+18)]
+        crop_img = image[(int)(height/2-box_height):(int)(height/2+box_height), (int)(width/2-box_width):(int)(width/2+box_width)]
         crop_gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-        # equi_gray = cv2.equalizeHist(crop_gray)
-        clipLimit = 10.0
-        tileGridSize = (4,4)
+        invert_img = cv2.bitwise_not(crop_gray)
+        clipLimit = 40.0
+        tileGridSize = (1,1)
         clahe = cv2.createCLAHE(clipLimit, tileGridSize)
-        equi_gray = clahe.apply(crop_gray)
-        kernel = np.ones((7,7),np.uint8)
-        opening_gray = cv2.morphologyEx(equi_gray, cv2.MORPH_OPEN, kernel)
-        invert_img = cv2.bitwise_not(opening_gray)
-
-        h, w = invert_img.shape
+        equi_gray = clahe.apply(invert_img)
+        kernel = np.ones((3,3),np.uint8)
+        erode_gray = cv2.erode(equi_gray, kernel, iterations = 2)
+        
+        # make padded image
+        padded_img = cv2.copyMakeBorder(erode_gray, top=0, bottom=0, left=box_height-box_width+0, right=box_height-box_width+0, borderType= cv2.BORDER_CONSTANT, value=[0] ) 
+       
+        h, w = padded_img.shape
         for i in range(h):
             for j in range(w):
-                if invert_img.item(i,j) < 150:
-                    invert_img.itemset((i,j),0)
-        
-        # show cropped frame
+                if padded_img.item(i,j) < 90:
+                    padded_img.itemset((i,j),0)
+
+        # invert for CNN
+        #cnn_img = cv2.bitwise_not(padded_img)
+
+        # show images
         cv2.rectangle(image,(width//2-18,height//2-30),(width//2+18,height//2+30),(0,0,255),3,-1)
         cv2.imshow("Frame", image)
+        cv2.imshow("gray", crop_gray)
+        cv2.imshow("inverted", invert_img)
+        cv2.imshow("equilized", equi_gray)
+        cv2.imshow("eroded", erode_gray)
+        cv2.imshow("padded", padded_img)
+        #cv2.imshow("cnn", cnn_img)
+
         key = cv2.waitKey(1) & 0xFF
-        cv2.imshow('invert_img', invert_img)
 
         # make letter prediction
-        prediction = predict(invert_img, model, mapping)
+        prediction = predict(padded_img, model, mapping)
         print(prediction)
 
         # activate pins for letter prediction if confidence is above certain threshold
-        if float(prediction["confidence"]) >= 70:
+        if float(prediction["confidence"]) >= 60:
             letter_to_pins(prediction["prediction"])
     
         # clear the stream in preparation for the next frame
